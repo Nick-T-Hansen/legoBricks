889c2f06ac931dd4ccf76b258ca453226b151eaa
Nick Hansen <nick.hansen13@gmail.com>
2018-12-14 08:32:08 -0600

    added new json object (colors) and created a dropdown menu with colors listed in it which coorespond to the json object.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,13 @@
 4- create object to represent a lego (eventlisteners.js)
 5- set up database for our legos (database.json) initializing with { "legos": []} is how we will find it on our local server localhost:8088/legos
 6- save data to the database (eventlisteners.js and data.js)
-
 */
+
+/* PRACTICE: create a color object which pulls available colors form a JSON and populates a dropdown menu  the brick form
+populate color array(done)
+create a function which appends drop down options based on the number of colors in the color object
+append the event listener for the color field changes
+ */
 
 
 domBuilder.appendInputForm();
